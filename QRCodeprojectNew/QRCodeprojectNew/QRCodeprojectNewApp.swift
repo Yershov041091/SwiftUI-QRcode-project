e0ca7562601d3087c6ac010103a2b3e1f866b417
Artem Yershov <ershov041091@icloud.com>
2023-04-30 11:54:21 +0300

    I've create QRcode project

---
+++ QRCodeprojectNew/QRCodeprojectNew/QRCodeprojectNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeprojectNewApp.swift
+//  QRCodeprojectNew
+//
+//  Created by Artem Yershov on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeprojectNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
